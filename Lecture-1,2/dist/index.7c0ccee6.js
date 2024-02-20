900910bed4949708f6a0ea6f0b6a941cc42cf792
Kushal Lalwani <kushallalwani.kl@gmail.com>
2024-02-20 15:08:18 +0530

    first commit

---
+++ Lecture-1,2/dist/index.7c0ccee6.js
@@ -0,0 +1,12 @@
+const heading1 = React.createElement("h1", {}, "Heading1");
+const heading2 = React.createElement("h2", {}, "Heading2");
+const div = React.createElement("div", {
+    id: "container"
+}, [
+    heading1,
+    heading2
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(div);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
